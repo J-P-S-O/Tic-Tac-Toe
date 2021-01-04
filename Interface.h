85c29935a2db3a0df07f22cba5408d9bef23992b
JPSO <jpso.guerreiro@gmail.com>
2021-01-04 10:29:29 -0300

    .

--- Interface.h
+++ Interface.h
@@ -1,5 +1,7 @@
 class Console (){
-	
+	public:             // Access specifier
+    	int myNum;        // Attribute (int variable)
+		string myString;
 }
 // The interface for printing, input and everything else
 
