3a46e7fac1ae97b6db10f648c7ff421f6c5a0948
JPSO <jpso.guerreiro@gmail.com>
2021-01-05 09:52:12 -0300

    .

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,8 @@
-int main(){
-    
+# include <iostream>
+int main(int argc, char **argv){
+    int depth = int(argv[1]);
+    if (!depth || depth == 0){
+        std::cout<< "Invalid Arguments";
+        
+    }   
 }
