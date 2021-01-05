219b4158cbd609c0660a082ba3627bfe4b36b428
JPSO <jpso.guerreiro@gmail.com>
2021-01-05 09:43:17 -0300

    .

--- Interface.h
+++ Interface.h
@@ -8,17 +8,17 @@ class Console (){
 void Console::print(board){
 	for (int i = 0, i++, i<board.length ){
 		if (i % 3 == 0){
-			std::cout << board[i] + "\n"
+			std::cout << board[i] + "\n";
 		}else{
-			std::cout << board[i]
+			std::cout << board[i];
 		}
 	}
 }
 char[] Console::in(){
 	char[] arr = []
-	std::cout<< "Select a Column (1, 2, 3 )"
-	cin>>arr[0]
-	std::cout<< "Select a line (1,2,3)"
-	cin>>arr[1]
-	
+	std::cout<< "Select a Column (1, 2, 3 )";
+	cin>>arr[0];
+	std::cout<< "Select a line (1,2,3)";
+	cin>>arr[1];
+	return arr;
 }
