96161a4e1ddd200f18fd6f97bac3d367452be7ae
JPSO <jpso.guerreiro@gmail.com>
2021-01-05 09:35:48 -0300

    .

--- Interface.h
+++ Interface.h
@@ -1,17 +1,24 @@
 # include <iostream>
 class Console (){
-	public:  
-		
+	public:
 		void print()           // Access specifier
     	char[] in()
 }
 // The interface for printing, input and everything else
 void Console::print(board){
-	for (int i = board.length, i++, i<board.length ){
+	for (int i = 0, i++, i<board.length ){
 		if (i % 3 == 0){
 			std::cout << board[i] + "\n"
 		}else{
 			std::cout << board[i]
 		}
 	}
+}
+char[] Console::in(){
+	char[] arr = []
+	std::cout<< "Select a Column (1, 2, 3 )"
+	cin>>arr[0]
+	std::cout<< "Select a line (1,2,3)"
+	cin>>arr[1]
+	
 }
