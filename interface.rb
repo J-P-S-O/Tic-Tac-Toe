2621bd9329a256e15cdc0c30625b4de315c6e510
JPSO <jpso.guerreiro@gmail.com>
2021-01-06 09:35:48 -0300

    .

--- interface.rb
+++ interface.rb
@@ -8,9 +8,9 @@ end
 def cin()
   arr = []
   print("Choose a column, 1 - 2 - 3 :")
-  arr[0] = Number(gets.chomp)-1
+  arr[0] = Integer(gets.chomp)-1
   puts "Choose a line, 1 -2 -3 "
-  arr[1] = Number(gets.chomp) -1
+  arr[1] = Integer(gets.chomp) -1
   if (arr[0] > 2 || arr[1] > 2)
     puts "Invalid chars"
     arr = []
@@ -18,7 +18,8 @@ def cin()
     return arr
   end
   return arr
-
-
-
 end
+
+def check(board)
+  board.each do | i |
+    if (i[0])
