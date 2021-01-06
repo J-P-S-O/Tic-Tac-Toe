da14abde098bf5040ff88e3daadd92b75ea0964c
JPSO <jpso.guerreiro@gmail.com>
2021-01-06 09:08:09 -0300

    .

--- interface.rb
+++ interface.rb
@@ -3,5 +3,14 @@ def cout(board)
   for (i in board):
     print([i[0], i[1], i[2]].join("||"))
 end
-def cin(board)
+def cin()
+  arr = []
   print("Choose a column, 1 - 2 - 3 :")
+  arr[0] = Number(gets.chomp)-1
+  puts "Choose a line, 1 -2 -3 "
+  arr[1] = Number(gets.chomp) -1
+  if (arr[0] > 2 || arr[1] > 2)
+    puts "Invalid chars"
+    arr = []
+    arr = cin()
+    return arr
