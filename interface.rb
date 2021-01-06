e541269c95768c0975f370458b3e695cd05e9ec6
JPSO <jpso.guerreiro@gmail.com>
2021-01-06 08:52:27 -0300

    .

--- interface.rb
+++ interface.rb
@@ -1,1 +1,7 @@
-# This file has the interface library 
+# This file has the interface library
+def cout(board):
+  for (i in board):
+    print([i[0], i[1], i[2]].join("||"))
+end
+def cin(board):
+  
