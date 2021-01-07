288aa9abcb7ed055ad8b1bbcc9b01366c38c7188
J-p-s-o <jpso.guerreiro@gmail.com>
2021-01-07 10:56:08 -0300

    .

--- Main.rb
+++ Main.rb
@@ -6,16 +6,5 @@ board = [
   ['','','']
 ]
 
-=begin
-# uncomment to test
-board = [
-  ['X','X','X'],
-  ['X','X','X'],
-  ['X','X','X']
-]
-=end
 require "./interface"
 require "./lib"
-test = cin()
-puts test.join("  ")
-cout(board)
---
+++ test.rb
@@ -0,0 +1,18 @@
+# this file is used for testing
+
+board = [
+  ['','',''],
+  ['','',''],
+  ['','','']
+]
+=begin
+# uncomment to test
+board = [
+  ['X','X','X'],
+  ['X','X','X'],
+  ['X','X','X']
+]
+=end
+test = cin()
+puts test.join("  ")
+cout(board)
