5e370a40738c34a09ed6ffba865f4d82a4d4a862
J-p-s-o <jpso.guerreiro@gmail.com>
2021-01-07 10:24:52 -0300

    .

--- interface.rb
+++ interface.rb
@@ -19,11 +19,3 @@ def cin()
   end
   return arr
 end
-
-def check(board)
-  board.each do | i |
-    if (i[0] == i[1] && i[1] == i[2])
-      return true
-    end
-  end
-end
--- lib.rb
+++ lib.rb
@@ -1,1 +1,8 @@
 # And this  file has the main stuff
+def check(board)
+  board.each do | i |
+    if (i[0] == i[1] && i[1] == i[2])
+      return true
+    end
+  end
+end
