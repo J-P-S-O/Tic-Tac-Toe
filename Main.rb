ddede0972ae0c64f6d171c354e2bb5d2eb8c73c2
JPSO <jpso.guerreiro@gmail.com>
2021-01-15 14:31:50 +0000

    .

--- Main.rb
+++ Main.rb
@@ -40,8 +40,50 @@ def check(board)
 end
 # SCORE AI
 def getScore(board)
-    
-    
+    cpl = getCurrentPlayer(board)
+=begin
+  # this function is the one that uses minimax to determine if
+  a board state is ultimately winner or not
+  It would be better if we used a `score` mechanism
+=end
+  outcomes = _moves(board)
+=begin
+  puts "debug from line isWinner #1"
+  puts outcomes.length()
+  outcomes.each do | out |
+    puts "# debug outcome "
+    cout(out)
+  end
+=end
+  # separates outcomes
+  wins = []
+  loss = []
+  tie = []
+  # main loop
+  outcomes.each do | outcome |
+    if (check(outcome)) # If this state is final
+        if (check(outcome) ==  cpl) # either it is won
+            wins.push(outcome)
+        elsif (check(outcome) == true)
+            tie.push(outcome)
+        else
+            loss.push(outcome)
+        end
+    else
+        if (!isWinner(outcome)) # uses recursive, but reverse
+            wins.push(outcome)
+        else
+            loss.push(outcome) #same thing
+        end
+    end
+   # now all possibilities are sorted
+   # we just need to figure out if it is more possible that we win or lose
+   if (wins.length() >= loss.length())
+     return true
+   end
+   return false
+
+       
 
 end 
 
